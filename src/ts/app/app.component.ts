91bdeb0fe322e6cc00b3692c8f6b36ff50d498fa
Freddy Duran <duran.freddy.g@gmail.com>
2017-08-24 13:34:41 -0400

    Add input field
    Signed-off-by: Freddy Duran <duran.freddy.g@gmail.com>

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -5,20 +5,35 @@ import { Component } from "@angular/core";
     template: `
     <h1>{{header}}</h1>
     <div>
+        <form>
+            <div>
+                <label for="new-color-input">New Color:</label>
+                <input type="text" id="new-color-input" 
+                    name="newColor" [(ngModel)]="newColor">
+            </div>
+            <button type="button" (click)="addColor()">Add Color</button>
+        </form>
         <ul>
-            <li *ngFor="let color of colors">            
-                {{color}}
-            </li>
-        </ul>
+        <li *ngFor="let color of colors">            
+            {{color}}
+        </li>
+    </ul>
     </div>
     `,
 })
 export class AppComponent {
 
-    public header: string = "Color Selector";
+    public header: string = "Color Tools";
 
     public colors: string[] = [
         "red", "white", "green", "orange", "blue",
     ]
+
+    public newColor: string = "";
+
+    public addColor() {
+        this.colors.push(this.newColor);
+        this.newColor = "";
+    }
 
 }
