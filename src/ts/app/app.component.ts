01ae964ce36a4b01f9f60fb77196c84f6554f258
Freddy Duran <duran.freddy.g@gmail.com>
2017-08-24 14:57:42 -0400

    Separar html de typescript
    Signed-off-by: Freddy Duran <duran.freddy.g@gmail.com>

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,24 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: `
-    <h1>{{header}}</h1>
-    <div>
-        <form>
-            <div>
-                <label for="new-color-input">New Color:</label>
-                <input type="text" id="new-color-input" 
-                    name="newColor" [(ngModel)]="newColor">
-            </div>
-            <button type="button" (click)="addColor()">Add Color</button>
-        </form>
-        <ol>
-            <li *ngFor="let color of colors">            
-                {{color}}
-            </li>
-        </ol>
-    </div>
-    `,
+    template: require("./app.component.html"),
 })
 export class AppComponent {
 
